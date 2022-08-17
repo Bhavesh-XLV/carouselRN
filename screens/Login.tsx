f666ad8fa28cb534f10041d9fa823b1c3756fd1f
Bhavesh <bhaveshagrawal123.ba@gmail.com>
2022-08-17 15:43:10 +0530

    login class component

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,62 +1,83 @@
 import React, {useState} from 'react';
 import {Alert, Button, StyleSheet, Text, TextInput, View} from 'react-native';
-const Login = ({navigation}: any) => {
-  const [email, setEmail] = useState<any>('');
-  const [password, setPassword] = useState<any>('');
-  const emailValidater = (validateEmail: any) => {
+
+type LoginProp = {
+  email: any;
+  password: any;
+};
+
+type AppProp = {
+  navigation: any;
+};
+
+class Login extends React.Component<AppProp> {
+  constructor(props: AppProp) {
+    super(props);
+  }
+  state: LoginProp = {
+    email: '',
+    password: '',
+  };
+  emailValidater = (validateEmail: any) => {
     if (validateEmail.endsWith('@gmail.com')) {
       return true;
     }
     return false;
   };
-  const passwordValidater = (validatePassword: any) => {
+  passwordValidater = (validatePassword: any) => {
     const check = new RegExp(
       '((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]))(?=.*[@#$%^&*]).{8,}',
     );
     return check.test(validatePassword);
   };
-  const submitHandler = () => {
-    if (emailValidater(email) && passwordValidater(password)) {
-      navigation.navigate('Profile');
+  submitHandler = () => {
+    if (
+      this.emailValidater(this.state.email) &&
+      this.passwordValidater(this.state.password)
+    ) {
+      Alert.alert('Successful');
+      this.props.navigation.navigate('Profile');
     } else {
       Alert.alert('Oops!', 'Enter valid Email and Password');
     }
   };
-  const profileHandler = () => {
-    navigation.navigate('Profile');
+  profileHandler = () => {
+    this.props.navigation.navigate('Profile');
   };
-  const carouselHandler = () => {
-    navigation.navigate('Carousel');
+  carouselHandler = () => {
+    this.props.navigation.navigate('Carousel');
   };
-  return (
-    <View style={styles.LoginScreen}>
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={enteredText => setEmail(enteredText)}
-        placeholder="Enter Email"
-        placeholderTextColor="black"
-      />
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={enteredText => setPassword(enteredText)}
-        placeholder="Enter Password"
-        placeholderTextColor="black"
-      />
-      <View style={styles.button}>
-        <Button title="Submit" onPress={submitHandler} />
+  render() {
+    return (
+      <View style={styles.LoginScreen}>
+        <TextInput
+          style={styles.input}
+          value={this.state.email}
+          onChangeText={enteredText => this.setState({email: enteredText})}
+          placeholder="Enter Email"
+          placeholderTextColor="black"
+        />
+        <TextInput
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={enteredText => this.setState({password: enteredText})}
+          placeholder="Enter Password"
+          placeholderTextColor="black"
+        />
+        <View style={styles.button}>
+          <Button title="Submit" onPress={this.submitHandler} />
+        </View>
+        <View style={styles.button}>
+          <Button title="Profile" onPress={this.profileHandler} />
+        </View>
+        <View style={styles.button}>
+          <Button title="Carousel" onPress={this.carouselHandler} />
+        </View>
       </View>
-      <View style={styles.button}>
-        <Button title="Profile" onPress={profileHandler} />
-      </View>
-      <View style={styles.button}>
-        <Button title="Carousel" onPress={carouselHandler} />
-      </View>
-    </View>
-  );
-};
-<TextInput />;
+    );
+  }
+}
+
 export default Login;
 
 const styles = StyleSheet.create({
